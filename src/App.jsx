e2f6906ba8a4f1e78783c39c742cf501b802c0ec
Ivan Kuria <ivankuria7@gmail.com>
2024-10-24 20:42:09 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import ToDoList from "./ToDoList"
+import "./index.css"
+
+function App() {
+
+  return (
+    <>
+      <ToDoList/>
+    </>
+  )
+}
+
+export default App
